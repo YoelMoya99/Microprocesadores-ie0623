0adecb3332d7670e82fc66148c4ba97e4f95fbb4
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2023-10-09 22:54:57 -0600

    Subrutina ASCII_BIN funcional y probada para los 20 datos

--- Tarea 3/YoelMoya_main_T3.asm
+++ Tarea 3/YoelMoya_main_T3.asm
@@ -10,6 +10,9 @@ FINMSG:                EQU $0
 
                 org $1000
 CANT:           ds 1
+CONT:                ds 1
+Offset:                ds 1
+ACC:                ds 2
 
 
                 org $1030
@@ -20,12 +23,144 @@ MSG_ENTER:      FCC " "
                 db CR,LF
                 db FINMSG
 
+
+                org $1500
+Datos_IoT:        fcc "0129"
+                fcc "0749"
+                fcc "3854"
+                fcc "1975"
+                fcc "0077"
+                
+                fcc "1346"
+                fcc "5343"
+                fcc "2575"
+                fcc "2023"
+                fcc "0398"
+
+                fcc "0000"
+                fcc "1239"
+                fcc "1873"
+                fcc "0005"
+                fcc "2084"
+
+                fcc "0064"
+                fcc "0128"
+                fcc "0256"
+                fcc "0512"
+                fcc "4095"
+
+
+                org $1600
+Datos_BIN:        ds 25
+
 ;----------------- PROGRAMA PRINCIPAL --------------------;
 
                 org $2000
+                lds #$3BFF
 
                 jsr GET_CANT
+
+                leas -2,sp
+
+                ldab #$00
+                pshb
+
+                ldd #Datos_BIN
+                pshd
+
+                ldd #Datos_IoT
+                pshd
+
+                leas 7,sp
+                jsr ASCII_BIN                
+
                 bra *
+
+
+
+;---------------------------------------------------------;
+;----------------- SUBRUTINA ASCII BIN -------------------;
+;---------------------------------------------------------;
+
+ASCII_BIN:
+                leas -5,sp
+
+                bclr CONT,$FF
+                bclr Offset,$FF
+
+ascii_loop:        pulx
+
+Decada_4:        ldaa Offset
+                ldab a,x
+                adda #1
+                staa Offset
+
+                subb #$30
+                ldaa #$00
+                ldy #1000
+                emul
+
+                std ACC
+
+Decada_3:        ldaa Offset
+                ldab a,x
+                adda #1
+                staa Offset
+
+                subb #$30
+                ldaa #$00
+                ldy #100
+                emul
+
+                addd ACC
+                std ACC
+        
+Decada_2:        ldaa Offset
+                ldab a,x
+                adda #1
+                staa Offset
+
+                subb #$30
+                ldaa #$00
+                ldy #10
+                emul
+
+                addd ACC
+                std ACC
+
+Decada_1:        ldaa Offset
+                ldab a,x
+                adda #1
+                staa Offset
+
+                subb #$30
+                ldaa #$00
+
+                addd ACC
+                std ACC
+
+guarda_val:        pshx
+                leas 2,sp
+
+                puly
+                pulb
+
+                ldx ACC
+                stx b,y
+
+                addb #$02
+                pshb
+                pshy
+
+                leas -2,sp
+                inc CONT
+                ldaa CONT
+
+                cmpa CANT
+                bne ascii_loop
+
+                leas 5,sp
+                rts
 
 ;---------------------------------------------------------;
 ;----------------- SUBRUTINA GET CANT --------------------;

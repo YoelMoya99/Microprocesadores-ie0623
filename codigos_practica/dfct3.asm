c760c123f51abfc27c42c7ec746b5f7048ba9987
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2023-10-18 12:53:29 -0600

    Modulo funcional
    Archivo funcional, falta banco de pruebas para comprobar funcionamiento. Documentacion terminada por el momento.

--- codigos_practica/dfct3.asm
+++ codigos_practica/dfct3.asm
@@ -10,85 +10,88 @@
 ;---------------------------------------------------------------------;
 
                         org $1000
-BCD:                        dW $9999
-NUM_BIN:                ds 2
+BCD:                    dW $9999
+Num_BIN:                ds 2
 
                         org $1010
-COUNT:                        ds 1
+COUNT:                  ds 1
 
                         org $1020
-TEMP1:                        ds 2
-BIN_Hi:                        ds 1
-BIN_Lo:                        ds 1
-BCDptr:                        EQU #TEMP1
-
+Temp:                   ds 2
 ;---------------------------------------------------------------------;
 ;----------------------- Programa Principal --------------------------;
 ;---------------------------------------------------------------------;
 
                         org $2000
-                        bclr BIN_Hi,$FF
-                        bclr BIN_Lo,$FF
-                        movb #13, COUNT
-                        movw BCD,TEMP1
-                        dec COUNT
-
-
-for_loop:                ldx #TEMP1
-                        lsr 0,x
-                        ror 1,x
-                        ror BIN_Hi
-                        ror BIN_Lo
+                        ldx #Temp
+                        ldy #Num_BIN
                         
+                        bclr 0,y,$FF ;No es nesesario del todo, agregado
+                        bclr 1,y,$FF ;Como extra para leer mejor valores
 
-                        ldaa 0,x
-                        anda #$F0
-			ldab #$F0
+                        movb #13,COUNT ;Inicializacion Contador Principal
+                        movw BCD,Temp  ;Carga valor a Rotar en variable temp
+
+
+
+
+for_loop:               lsr 0,x ;Rotacion hacia la derecha de valor BCD y
+                        ror 1,x ;Num_BIN. Rotacion conjunta de 2 words,
+                        ror 0,y ;desplazando con cero entrante y rotanto con
+                        ror 1,y ;carry los 3 bytes restantes
+                        
+                        ;PARA LAS DOS DECADAS MAS SIGNIFICATIVAS
+                        ldd #$F00F ;Carga mascara para aislar DEC alta y baja
+                        anda 0,x   ;Aisla decada alta
+                        andb 0,x   ;Aisla decada baja
+
+                        ;RESTA 30 SI DEC ALTA MAYOR O IGUAL QUE 80
                         cmpa #$80
                         blo Hi_No_Sub_30
                         suba #$30
-Hi_No_Sub_30:                
-                        andb #$0F
+Hi_No_Sub_30:
+                        ;RESTA 3 SI DEC BAJA MAYOR O IGUAL QUE 8
                         cmpb #$08
                         blo Hi_No_Sub_03
-                        subb #$30                
+                        subb #$03
 Hi_No_Sub_03:
-                        aba
-                        staa 0,x
+                        aba ;Une las decadas filtradas
+                        staa 0,x ;guarda las decadas filtradas
 
-                        
-                        ldaa 1,x
-                        anda #$F0
-			ldab 1,x
-                        cmpa #$80
-                        blo Hi_No_Sub_30
+                        ;PARA LAS DOS DECADAS MENOS SIGNIFICATIVAS
+                        ldd #$F00F ;Carga mascara denuevo
+                        anda 1,x ;Aisla decada mas significativa
+                        andb 1,x ;Aisla decada menos significativa
+
+                        ;RESTA 30 SI LA DEC ALTA MAYOR O IGUAL QUE 80
+			cmpa #$80
+                        blo Lo_No_Sub_30
                         suba #$30
-Lo_No_Sub_30:                
-                        andb #$0F
+Lo_No_Sub_30:
+                        ;RESTA 3 SI LA DEC BAJA MAYOR O IGUAL QUE 8
                         cmpb #$08
-                        blo Hi_No_Sub_03
-                        subb #$30                
+                        blo Lo_No_Sub_03
+                        subb #$03
 Lo_No_Sub_03:
-                        aba
-                        staa 1,x
+                        aba ;Une las decadas filtradas
+                        staa 1,x ;Guarda las decadas filtradas
 
-                        dec COUNT
-                        ldaa COUNT
-                        cmpa #$00
+
+                        dec COUNT ;Falta un desplazamiento con comparacion
+                        tst COUNT ;menos. Si no hemos terminado, salte
                         bne for_loop
                         
-                        movb #3,COUNT
+                        movb #3,COUNT ;Carga segundo contador para los
+                                      ;desplazamientos sin comparacion
 
-Optimizacion:                lsr 0,x
-				ror 1,x
-                        ror BIN_Hi
-                        ror BIN_Lo
+                        ;DESPLAZAMIENTOS SIN COMPARACION:
+Optimizacion:           lsr 0,x
+                        ror 1,x
+                        ror 0,y
+                        ror 1,y
                         
-                        dec COUNT
-                        ldaa COUNT
-                        cmpa #$00
+                        dec COUNT ;Falta un despl sin comparacion menos
+                        tst COUNT ;Si no hemos terminado, salte
                         bne Optimizacion
                         
-                        movb TEMP1,NUM_BIN
-
-                        bra *
+                        bra * ; Ya termino, revisar resultado en $1002

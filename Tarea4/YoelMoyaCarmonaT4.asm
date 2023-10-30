f8569e9f0b3f5f001bd1485a9cf0a04bd4c56f6b
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2023-10-30 12:52:40 -0600

    Funciono una vez
    Falta realizar plan de pruebas pero no puedo evitar verlo como algo prometedor!

--- Tarea4/YoelMoyaCarmonaT4.asm
+++ Tarea4/YoelMoyaCarmonaT4.asm
@@ -15,6 +15,7 @@
 ;--- Aqui se colocan los valores de carga para los timers de la aplicacion ----
 
 tSupRebPB:        EQU 10
+tSupRebTCL:          EQU 10    ;Timer de sup rebotes para TCL, 10ms
 tShortP:          EQU 25
 tLongP:           EQU 3
 tTimer1mS:        EQU 1     ;Base de tiempo de 1 mS (1 ms x 1)
@@ -29,12 +30,25 @@ MaskPB            EQU $01
                                 Org $1000
 
 ;Aqui se colocan las estructuras de datos de la aplicacion
+MAX_TCL:          dB $04
+Tecla:            ds 1
+Tecla_IN:         ds 1
+Cont_TCL:         ds 1
+Patron:           ds 1
+Est_Pres_TCL:     ds 2
 Est_Pres_LeerPB:  ds 2
-Banderas_PB:      ds 1
 
+Banderas_PB:      ds 1
 ShortP:           EQU $01
 LongP:            EQU $02
-                                
+ARRAY_OK:          EQU $04
+
+                  org $1010
+Num_Array:        ds 10
+
+                  org $1020
+Teclas:           dB $01,$02,$03,$04,$05,$06,$07,$08,$09,$00,$0E,$0B
+
 ;===============================================================================
 ;                              TABLA DE TIMERS
 ;===============================================================================
@@ -51,6 +65,7 @@ Timer10mS:      ds 1       ;Timer para generar la base de tiempo 10 mS
 Timer1_Base1:   ds 1       ;Ejemplos de timers de aplicacion con BaseT
 Timer2_Base1:   ds 1
 Timer_RebPB:    ds 1
+Timer_RebTCL:        ds 1        ;Timer de supresion de rebotes
 
 Fin_Base1mS:    dB $FF
 
@@ -93,6 +108,9 @@ Fin_Base1S        dB $FF
         Movb #$0F,DDRP    ;bloquea los display de 7 Segmentos
         Movb #$0F,PTP
         
+        movb #$F0,DDRA    ;Define el puerto A como salida y entrada p/teclado
+        bset PUCR,$01     ;Activa las resistencias de pullup del puerto A
+
         Movb #$17,RTICTL   ;Se configura RTI con un periodo de 1 mS
         Bset CRGINT,$80
 ;===============================================================================
@@ -104,21 +122,153 @@ Fin_Base1S        dB $FF
         Movb #tTimer1S,Timer1S
         Movb #tTimerLDTst,Timer_LED_Testigo  ;inicia timer parpadeo led testigo
 
-	Lds #$3BFF
+        movb #$FF,Tecla    ;Inicializa el valor de tecla
+        movb #$FF,Tecla_IN ;Inicializa el valor de tecla_in
+        movb #$00,Cont_TCL ;Inicializa offset en cero
+        movb #$00,Patron   ;Inicializa patron del lectura teclado
+
+        ldx #Num_Array
+        movb #$09,Cont_TCL
+for:    movb #$FF,1,x+
+        dec Cont_TCL
+        bne for
+        movb #$00,Cont_TCL
+
+        Lds #$3BFF
         Cli
         Clr Banderas_PB
         Movw #LeerPB_Est1,Est_Pres_LeerPB
-        
+        Movw #Teclado_Est1,Est_Pres_TCL
+
 Despachador_Tareas
 
         Jsr Tarea_Led_Testigo
         ; Aqui se colocan todas las tareas del programa de aplicacion
+        Jsr Tarea_Teclado
         Jsr Tarea_Leer_PB
         Jsr Tarea_LED_PB
         
         Bra Despachador_Tareas
        
 
+;******************************************************************************
+;                               TAREA TECLADO
+;******************************************************************************
+
+Tarea_Teclado:
+                        ldx Est_Pres_TCL
+                        jsr 0,x
+                        rts
+
+;------------------------------ Teclado Est1 ---------------------------------
+
+Teclado_Est1:           ;movb #$02,PORTB ;Comprobacionestado1
+                        jsr Leer_Teclado
+                        
+                        ldaa #$FF
+                        cmpa Tecla
+                        beq Fin_Teclado_Est1
+
+                        movb #tSupRebTCL,Timer_RebTCL
+                        movw #Teclado_Est2,Est_Pres_TCL
+                        movb Tecla,Tecla_IN
+
+Fin_Teclado_Est1:        rts
+
+;---------------------------- Teclado Est2 -----------------------------------
+
+Teclado_Est2:           ;movb #$04,PORTB ;Comprobacion estado2
+                        tst Timer_RebTCL
+                        bne Fin_Teclado_Est2
+
+                        jsr Leer_Teclado
+
+                        ldaa Tecla_IN
+                        cmpa Tecla
+                        beq Next_TCL_Est_3
+
+                        movw #Teclado_Est1,Est_Pres_TCL
+                        bra Fin_Teclado_Est2
+
+Next_TCL_Est_3:
+                        movw #Teclado_Est3,Est_Pres_TCL
+
+Fin_Teclado_Est2:        rts
+
+
+;--------------------------- Teclado Est 3 ----------------------------------
+
+Teclado_Est3:           ;movb #$08,PORTB
+                        jsr Leer_Teclado
+                        
+                        ldaa Tecla_IN
+                        cmpa Tecla
+                        beq Sigue_Presionada
+
+                        movw #Teclado_Est4,Est_Pres_TCL
+                        movw Tecla_IN,Tecla
+                        bra Fin_Teclado_Est3
+
+Sigue_Presionada:
+                        movw #Teclado_Est3,Est_Pres_TCL
+
+Fin_Teclado_Est3:        rts
+
+;---------------------------- Teclado Est 4 --------------------------------
+
+Teclado_Est4:           ;movb #$10,PORTB ;Hecho para ver transicino de estados
+                        ldab MAX_TCL
+                        cmpa Cont_TCL
+                        beq Long_Max
+
+No_Long_Max:                tst Cont_TCL
+                        beq Array_Zero
+
+Array_No_Zero:                ldaa #$0B
+                        cmpa Tecla
+                        beq Borrar_Valor
+                        ldaa #$0E
+                        cmpa Tecla
+                        beq Finalizar_Array
+                        bra Agregar_Valor
+
+Array_Zero:                ldaa #$0B
+                        cmpa Tecla
+                        beq Reestablecer
+                        ldaa #$0E
+                        cmpa Tecla
+                        beq Reestablecer
+                        bra Agregar_Valor
+
+Long_Max:                ldaa #$0B
+                        cmpa Tecla
+                        beq Borrar_Valor
+                        ldaa #$0E
+                        cmpa Tecla
+                        beq Finalizar_Array
+                        bra Reestablecer
+                        
+Agregar_Valor:                ldab Cont_TCL
+                        ldy #Num_Array
+                        movb Tecla,b,y
+                        inc Cont_TCL
+                        bra Reestablecer
+                        
+Borrar_Valor:                dec Cont_TCL
+                        ldab Cont_TCL
+                        ldy #Num_Array
+                        movb #$FF,b,y
+                        bra Reestablecer
+
+Finalizar_Array:        bclr Cont_TCL,#$FF
+                        bset Banderas_PB,ARRAY_OK
+
+Reestablecer:                movw #Teclado_Est1,Est_Pres_TCL
+                        bset Tecla,#$FF
+                        bset Tecla_IN,#$FF
+
+
+Fin_Teclado_Est4:        rts
 
 ;******************************************************************************
 ;                               TAREA LEER PB
@@ -191,6 +341,50 @@ Label_Short:            brclr PortPB,MaskPB,PBEst4_Retornar
 PBest4_State1:          movw #LeerPB_Est1,Est_Pres_LeerPB
 
 PBEst4_Retornar:        rts
+
+
+;*****************************************************************************
+;                      SUB RUTINA GENERAL LEER TECLADO
+;*****************************************************************************
+
+Leer_Teclado:
+                        ldab #$00
+                        movb #$EF,Patron
+
+Next_Row:
+                        movb Patron,PORTA
+                        nop
+                        nop
+                        nop
+
+                        brclr PORTA,$01,Column_1
+                        brclr PORTA,$02,Column_2
+                        brclr PORTA,$04,Column_3
+                        cmpb #$09
+                        beq No_Valor
+                        bra Prep_Next_Row
+Column_1:
+                        ldy #Teclas
+                        movb b,y,Tecla
+                        bra End_Leer_Teclado
+Column_2:
+                        ldy #Teclas
+                        addb #$01
+                        movb b,y,Tecla
+                        bra End_Leer_Teclado
+Column_3:
+                        ldy #Teclas
+                        addb #$02
+                        movb b,y,Tecla
+                        bra End_Leer_Teclado
+Prep_Next_Row:
+                        rol Patron
+                        addb #$03
+                        bra Next_Row
+No_Valor:
+                        movb #$FF,Tecla 
+
+End_Leer_Teclado:        rts
 
 ;*****************************************************************************
 ;                              TAREA LED PB
